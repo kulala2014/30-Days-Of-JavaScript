f75eea69e41ee5c98bdfe05b4b7813402a4544cc
kulala2014 <125820124@qq.com>
2022-01-27 10:14:04 +0800

    day1 learning

---
+++ 01_Day_Introduction/Exercises/datatypes.js
@@ -0,0 +1,15 @@
+//comments can make code readable
+//Welcome to 30DaysOfJavaScript
+
+/* comments can make code readable,
+    easy to resue and informative*/
+
+console.log(typeof day1Code)
+console.log(typeof learningFlag)
+console.log(typeof unLearning)
+console.log(typeof learningSteps)
+console.log(typeof learningDays)
+
+let myAge = 33, yourAge = 25
+console.log(`I am ${myAge} years old.`)
+console.log(`You are ${yourAge} years old.`)
---
+++ 01_Day_Introduction/Exercises/variable.js
@@ -0,0 +1,27 @@
+//variable file using to declare variable and assign values
+let day1Code = 'Welcome to 30DaysOfJavaScript'
+let learningFlag = true
+let unLearning = undefined
+let learningSteps = null
+const learningDays = 30
+
+
+let name,    //name of a person
+  job,
+  live,
+  address
+
+
+
+const githubUserName = 'kulala'
+const usrJob = 'engineer'
+const userAge = '33'
+const userAgent = 'China'
+
+let firstName = 'Clyde',
+    lastName = 'Gao',
+    maritalStatus = 'Yes',
+    country = 'China',
+    age = 33
+
+let first = 'Xixi', last = 'gao', marital = 'No', station = 'CN', old = 5
